dbdb10495ef9b5772b0dff06d7a2118579b79f65
이준혁 <ballack02@naver.com>
2023-02-23 15:00:26 +0900

    문제풀이

---
+++ baekjoon/Main.java
@@ -0,0 +1,25 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        Integer A = Integer.parseInt(st.nextToken());
+        Integer B = Integer.parseInt(st.nextToken());
+        Integer C = Integer.parseInt(st.nextToken());
+
+        System.out.println((A + B) % C);
+        System.out.println(((A % C ) + (B % C)) % C);
+        System.out.println((A * B) % C);
+        System.out.println(((A % C) * (B % C)) % C);
+    }
+}

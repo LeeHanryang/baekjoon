b33a90db62ef4d18cb02b99c07b1e29ed24aaaa9
이준혁 <ballack02@naver.com>
2023-02-23 17:44:35 +0900

    자원 해제

---
+++ baekjoon/2588.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer A = Integer.parseInt(br.readLine());
+        Integer B = Integer.parseInt(br.readLine());
+
+        String strB = String.valueOf(B);
+
+        for(int i = strB.length(); i > 0; i--){
+            Integer multi = A * Integer.parseInt(strB.substring(i - 1, i));
+            
+            System.out.println(multi);
+        }
+        
+        System.out.println(A * B);
+
+        br.close();
+    }
+}

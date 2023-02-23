359deffc4a91a678fc14c62d1c603c1d1170d42d
이준혁 <ballack02@naver.com>
2023-02-23 17:43:34 +0900

    2588문제 추가

--- baekjoon/Main.java
+++ baekjoon/Main.java
@@ -3,23 +3,22 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        Integer A = Integer.parseInt(br.readLine());
+        Integer B = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(str, " ");
+        String strB = String.valueOf(B);
 
-        Integer A = Integer.parseInt(st.nextToken());
-        Integer B = Integer.parseInt(st.nextToken());
-        Integer C = Integer.parseInt(st.nextToken());
-
-        System.out.println((A + B) % C);
-        System.out.println(((A % C ) + (B % C)) % C);
-        System.out.println((A * B) % C);
-        System.out.println(((A % C) * (B % C)) % C);
+        for(int i = strB.length(); i > 0; i--){
+            Integer multi = A * Integer.parseInt(strB.substring(i - 1, i));
+            
+            System.out.println(multi);
+        }
+        
+        System.out.println(A * B);
     }
 }

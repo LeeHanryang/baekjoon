955bbdacee84a28a3293d74787ed3c789c2f2d8e
이준혁 <ballack02@naver.com>
2023-02-28 15:04:23 +0900

    20230228

---
+++ baekjoon/if/9498.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int score = Integer.parseInt(br.readLine());
+
+        char grade;
+        
+        switch(score / 10) {
+        case 10:
+        case 9:
+            grade = 'A';
+            break;
+        case 8:
+            grade = 'B';
+            break;
+        case 7:
+            grade = 'C';
+            break;
+        case 6:
+            grade = 'D';
+            break;
+        default:
+            grade = 'F';
+            break;
+    }
+
+    System.out.println(grade);
+        
+    }
+}
